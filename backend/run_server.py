9ef201c7332a8986bc1ffe059db9651accfe073a
Yujiro Yahata <syani4732@gmail.com>
2020-01-18 11:45:48 +0900

    ファイル調整とREADME作成

--- backend/run_server.py
+++ backend/run_server.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+import os
 import flask
 import numpy as np
 import pandas as pd
@@ -9,9 +10,13 @@ from sklearn import svm
 from sklearn.model_selection import GridSearchCV
 import pickle
 import logging
+from flask_cors import CORS
+from werkzeug.utils import secure_filename
+import uuid
+import json
 
 # 設定
-TEST_FILE_NAME = "0_0_20191217132856JST_resample_All.csv"
+#TEST_FILE_NAME = "0_0_20191217132856JST_resample_All.csv"
 #TEST_FILE_NAME = "csvjson.json"
 MODEL_FILE_NAME = "model.binary"
 COLUMNS = ["AccX", "AccY", "AccZ", "GyroX", "GyroY", "GyroZ"]
@@ -21,14 +26,23 @@ MAX_TRACE = 10
 # initialize our Flask application and pre-trained model
 app = flask.Flask(__name__)
 app.logger.setLevel(logging.DEBUG)
+app.config['UPLOAD_FOLDER'] = './uploads'
+app.config['JSON_FOLDER'] = './jsons'
+CORS(app)
 
 
-@app.route("/test", methods=["POST"])
-def test():
-    json = flask.request.get_json()
-    # return flask.jsonify(json)
-    app.logger.debug('debug message')
-    return json['aa']
+@app.route("/get_json", methods=["GET"])
+def get_json():
+    response = {
+        "success": False,
+        "Content-Type": "application/json",
+    }
+    filename = flask.request.args.get('filename')
+    f = open(os.path.join(app.config['JSON_FOLDER'], filename), 'r')
+    json_to_dict_data = json.load(f)
+    response.update(json_to_dict_data)
+    response['success'] = True
+    return flask.jsonify(response)
 
 
 @app.route("/predict", methods=["POST"])
@@ -36,13 +50,19 @@ def predict():
     response = {
         "success": False,
         "Content-Type": "application/json",
+        "filename": ''
     }
 
     if flask.request.method == "POST":
         if True:
-            # read feature from json
-            #json = flask.request.get_json()
-            df_test = pd.read_csv(TEST_FILE_NAME, index_col=0)
+            csvFile = flask.request.files['file']
+            # uuidを元に一意なファイル名を作成
+            #filename = secure_filename(csvFile.filename)
+            filename = str(uuid.uuid4()) + '.csv'
+            csvFile.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+            filepath = app.config['UPLOAD_FOLDER'] + '/' + filename
+            df_test = pd.read_csv(filepath, index_col=0)
             df_test = df_test.loc[:, COLUMNS]
             df_test.columns = [col+"_0" for col in COLUMNS]
             for i in range(MAX_TRACE):
@@ -63,7 +83,13 @@ def predict():
 
             df_predict = pd.DataFrame(index=df_test.index)
             df_predict.loc[:, "predict"] = clf.predict(X_test)
-            response.update(df_predict.to_dict())
+
+            json_name = str(uuid.uuid4()) + '.json'
+            json_fw = open(os.path.join(
+                app.config['JSON_FOLDER'], json_name), 'w')
+
+            json.dump(df_predict.to_dict(), json_fw, indent=4)
+            response['filename'] = json_name
             response['success'] = True
 
     # return the data dictionary as a JSON response
@@ -72,4 +98,4 @@ def predict():
 
 if __name__ == "__main__":
     print(" * Flask starting server...")
-    app.run(debug=True)
+    app.run(debug=True, port=5000)
--- frontend/src/dashboard/Dashboard.tsx
+++ frontend/src/dashboard/Dashboard.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect, ChangeEvent} from 'react';
 import clsx from 'clsx';
 import {makeStyles} from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
@@ -19,10 +19,57 @@ import MenuIcon from '@material-ui/icons/Menu';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import NotificationsIcon from '@material-ui/icons/Notifications';
 import {mainListItems} from './listItems';
-import Chart from './Chart';
-import Judgements from './Judgements';
-import Conditions from './Conditions';
-import FileUploader from './FileUploader';
+
+import axios from 'axios';
+import Button from '@material-ui/core/Button';
+import CloudUploadIcon from '@material-ui/icons/CloudUpload';
+import DescriptionOutlinedIcon from '@material-ui/icons/DescriptionOutlined';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import Title from './Title';
+
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+
+import {useTheme} from '@material-ui/core/styles';
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  Label,
+  ResponsiveContainer,
+} from 'recharts';
+
+const API_URL = 'http://127.0.0.1:5000/';
+
+function createData(time: string, amount: number) {
+  return {time: time, amount: amount};
+}
+
+// ここに震えの値を入れる
+const exaData: ChartData[] = [
+  createData('10', 0),
+  createData('20', 0),
+  createData('30', 0),
+  createData('40', 0),
+  createData('50', 0),
+  createData('60', 0),
+  createData('70', 0),
+  createData('80', 0),
+];
+
+interface Predict {
+  key: string;
+  value: any;
+}
+
+interface ChartData {
+  time: string;
+  amount: number;
+}
 
 function Copyright() {
   return (
@@ -35,6 +82,10 @@ function Copyright() {
       {'.'}
     </Typography>
   );
+}
+
+function preventDefault(event: any) {
+  event.preventDefault();
 }
 
 const drawerWidth = 240;
@@ -116,11 +167,41 @@ const useStyles = makeStyles(theme => ({
   fixedHeight: {
     height: 240,
   },
+  fixedSecondHeight: {
+    height: '100%',
+  },
+  button: {
+    margin: theme.spacing(1),
+  },
+  input: {
+    display: 'none',
+  },
+  mt3: {
+    marginTop: theme.spacing(3),
+  },
+  seeMore: {
+    marginTop: theme.spacing(3),
+  },
+  depositContext: {
+    flex: 1,
+  },
 }));
 
 export default function Dashboard() {
   const classes = useStyles();
-  const [open, setOpen] = React.useState(false);
+  const theme = useTheme();
+
+  const [open, setOpen] = useState(false);
+  const [fileName, setFileName] = useState('');
+  const [data, setData] = useState(new FormData());
+  const [isLoading, setIsLoading] = useState(false);
+  const [uploaded, setUploaded] = useState(false);
+  const [jsonName, setJsonName] = useState('');
+  // const [predictedData, setPredictedData] = useState([{key: '', value: ''}]);
+  const [predictLoading, setPredictLoading] = useState(false);
+  const [chartData, setChartData] = useState(exaData);
+  const [isPositive, setIsPositive] = useState(false);
+
   const handleDrawerOpen = () => {
     setOpen(true);
   };
@@ -128,6 +209,127 @@ export default function Dashboard() {
     setOpen(false);
   };
   const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
+  const fixedSecondHeightPaper = clsx(classes.paper, classes.fixedSecondHeight);
+
+  const checkImageFileType = (type: string) => {
+    if (type === 'text/csv') {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  const onChangeFile = (event: ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files === null) {
+      return;
+    } else {
+      const files = event.target.files;
+      if (files.length > 0 && checkImageFileType(files[0].type)) {
+        const data = new FormData();
+        data.append('file', files[0]);
+        setFileName(files[0].name);
+        setData(data);
+      }
+      return;
+    }
+  };
+
+  const resetFile = () => {
+    setFileName('');
+    setData(new FormData());
+    return;
+  };
+
+  const fileUpload = async () => {
+    // CORS問題未解決
+    if (!(fileName && data.get('file'))) {
+      window.alert('ファイルを選択してください。');
+      return;
+    }
+    const axiosConfig = {
+      headers: {
+        'content-type': 'multipart/form-data',
+      },
+    };
+    setIsLoading(true);
+    const response = await axios.post(API_URL + 'predict', data, axiosConfig);
+    setIsLoading(false);
+    if (response.status !== 200) {
+      console.log('error');
+      window.alert('正常にファイルをアップロードできませんでした。');
+    } else {
+      setUploaded(true);
+      setJsonName(response.data.filename.toString());
+      return;
+    }
+  };
+
+  const allReset = (event: any) => {
+    event.preventDefault();
+    setFileName('');
+    setData(new FormData());
+    setUploaded(false);
+  };
+
+  useEffect(() => {
+    const httpGet = async () =>
+      await axios.get(API_URL + 'get_json', {
+        params: {
+          filename: jsonName,
+        },
+      });
+
+    if (jsonName !== '') {
+      if (uploaded) {
+        setPredictLoading(true);
+      }
+      httpGet()
+        .then(response => {
+          setPredictLoading(false);
+          let predicts: Predict[] = Object.entries(response.data.predict).map(
+            ([key, value]) => ({
+              key,
+              value,
+            })
+          );
+
+          let positiveNum = 0;
+          for (let i = 0; i < predicts.length; i++) {
+            if (predicts[i].value) {
+              positiveNum++;
+            }
+          }
+          if (2 * positiveNum >= predicts.length) {
+            setIsPositive(true);
+          } else {
+            setIsPositive(false);
+          }
+
+          let tmpChartData: ChartData[] = [];
+          let itr = 1,
+            tmpNum = 0;
+          for (let i = 0; i < predicts.length; i++) {
+            if (predicts[i].value) {
+              tmpNum++;
+            }
+
+            if (i >= 1000 * itr) {
+              tmpChartData.push({
+                time: (itr * 10).toString(),
+                amount: tmpNum,
+              });
+              tmpNum = 0;
+              itr++;
+            }
+          }
+          setChartData(tmpChartData);
+        })
+        .catch(error => {
+          setPredictLoading(false);
+          console.log(error);
+        });
+    }
+  }, [jsonName]);
 
   return (
     <div className={classes.root}>
@@ -187,25 +389,214 @@ export default function Dashboard() {
             {/* Chart */}
             <Grid item xs={12} md={7} lg={8}>
               <Paper className={fixedHeightPaper}>
-                <Chart />
+                <Title>今日の振動記録</Title>
+                <ResponsiveContainer>
+                  <BarChart
+                    data={chartData}
+                    margin={{
+                      top: 16,
+                      right: 16,
+                      bottom: 0,
+                      left: 24,
+                    }}
+                  >
+                    <XAxis
+                      dataKey="time"
+                      stroke={theme.palette.text.secondary}
+                    />
+                    <YAxis stroke={theme.palette.text.secondary}>
+                      <Label
+                        angle={270}
+                        position="left"
+                        style={{
+                          textAnchor: 'middle',
+                          fill: theme.palette.text.primary,
+                        }}
+                      >
+                        回数
+                      </Label>
+                    </YAxis>
+                    <Bar dataKey="amount" fill="#3f51b5" />
+                  </BarChart>
+                </ResponsiveContainer>
               </Paper>
             </Grid>
             {/* Recent Judgements */}
             <Grid item xs={12} md={5} lg={4}>
               <Paper className={fixedHeightPaper}>
-                <Judgements />
+                <Title>パーキンソン病判定</Title>
+                <Typography component="p" variant="h4">
+                  {uploaded
+                    ? isPositive
+                      ? '陽性'
+                      : '陰性'
+                    : 'CSVファイルをアップロードして下さい。'}
+                </Typography>
+                <Typography
+                  color="textSecondary"
+                  className={classes.depositContext}
+                >
+                  on 22 December, 2019
+                </Typography>
+                <div>
+                  <Link color="primary" href="#" onClick={preventDefault}>
+                    View detail
+                  </Link>
+                </div>
               </Paper>
             </Grid>
             {/* File Upload */}
             <Grid item xs={12} md={5} lg={6}>
-              <Paper className={classes.paper}>
-                <FileUploader />
+              <Paper className={fixedSecondHeightPaper}>
+                <Title>診断データアップロード</Title>
+                {uploaded ? (
+                  <div>
+                    <Typography component="p" variant="h5">
+                      データの分析が完了しました。
+                    </Typography>
+                    <Link color="primary" href="#" onClick={allReset}>
+                      別のファイルをアップロード
+                    </Link>
+                  </div>
+                ) : (
+                  <div>
+                    <Grid
+                      container
+                      alignItems="center"
+                      justify="center"
+                      className={classes.mt3}
+                    >
+                      <Grid item xs={12} md={6} lg={6}>
+                        <div className={classes.button}>
+                          <input
+                            accept="text/csv"
+                            className={classes.input}
+                            id="text-button-file"
+                            multiple
+                            type="file"
+                            onChange={onChangeFile}
+                          />
+                          <label htmlFor="text-button-file">
+                            <Button
+                              component="span"
+                              variant="contained"
+                              color="default"
+                              startIcon={<DescriptionOutlinedIcon />}
+                              fullWidth={true}
+                            >
+                              CSVを選択
+                            </Button>
+                          </label>
+                        </div>
+                      </Grid>
+                      <Grid item xs={12} md={6} lg={6}>
+                        <div className={classes.button}>
+                          <Button
+                            component="span"
+                            variant="contained"
+                            color="default"
+                            onClick={resetFile}
+                            fullWidth={true}
+                          >
+                            リセット
+                          </Button>
+                        </div>
+                      </Grid>
+                    </Grid>
+                    <Typography component="p" className={classes.button}>
+                      ファイル名: {fileName ? fileName : '選択なし'}
+                    </Typography>
+
+                    {isLoading ? (
+                      <Grid
+                        container
+                        direction="column"
+                        alignItems="center"
+                        justify="center"
+                      >
+                        <Grid item xs={12}>
+                          アップロード中
+                          <CircularProgress />
+                        </Grid>
+                      </Grid>
+                    ) : (
+                      <Grid
+                        container
+                        alignItems="center"
+                        justify="center"
+                        className={classes.mt3}
+                      >
+                        <Grid item xs={12}>
+                          <div className={classes.button}>
+                            <Button
+                              component="span"
+                              variant="contained"
+                              color="primary"
+                              startIcon={<CloudUploadIcon />}
+                              fullWidth={true}
+                              onClick={fileUpload}
+                            >
+                              アップロード
+                            </Button>
+                          </div>
+                        </Grid>
+                      </Grid>
+                    )}
+                  </div>
+                )}
               </Paper>
             </Grid>
             {/* Recent Conditions */}
             <Grid item xs={12} md={7} lg={6}>
-              <Paper className={classes.paper}>
-                <Conditions />
+              <Paper className={fixedSecondHeightPaper}>
+                <Title>最近の調子</Title>
+                {predictLoading ? (
+                  <Grid
+                    container
+                    direction="column"
+                    alignItems="center"
+                    justify="center"
+                  >
+                    <Grid item xs={12}>
+                      読み込み中
+                      <CircularProgress />
+                    </Grid>
+                  </Grid>
+                ) : (
+                  <Table size="small">
+                    <TableHead>
+                      <TableRow>
+                        <TableCell align="right">時間(s)</TableCell>
+                        <TableCell align="right">陽性/陰性</TableCell>
+                      </TableRow>
+                    </TableHead>
+
+                    {chartData !== exaData ? (
+                      <TableBody>
+                        {chartData.map(element => {
+                          return (
+                            <TableRow>
+                              <TableCell align="right">
+                                {element.time}
+                              </TableCell>
+                              <TableCell align="right">
+                                {element.amount * 2 >= 1000 ? '陽性' : '陰性'}
+                              </TableCell>
+                            </TableRow>
+                          );
+                        })}
+                      </TableBody>
+                    ) : (
+                      ''
+                    )}
+                  </Table>
+                )}
+
+                <div className={classes.seeMore}>
+                  <Link color="primary" href="#" onClick={preventDefault}>
+                    さらに見る
+                  </Link>
+                </div>
               </Paper>
             </Grid>
           </Grid>
